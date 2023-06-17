5d7f845d85f7e28b5588413f4ec90e8f28f3d80f
Rolvin Noronha <noronha102rolvin@gmail.com>
2023-06-17 09:39:42 +0530

    add dropdown

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 export default function Header() {
     const router = useRouter();
     const [isLogged, setIsLogged] = useState<boolean>(false);
+    const [dropdown, setDropdown] = useState<boolean>(false); 
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
@@ -26,11 +27,27 @@ export default function Header() {
         <Link className="header__title" href="/home">SocialMediaApp</Link>
         {
             isLogged && 
+            <>
             <div className="header__links">
                 <Link className="header__links-search" href="/search">Search</Link>
                 <Link className="header__links-profile" href="/my-profile">My Profile</Link>
                 <button onClick={logout} className="btn btn-header" >Sign out</button>
             </div>
+
+            <div className="dropdown">
+                <button onClick={() => setDropdown(!dropdown)} className="dropdown-btn">
+                    <p><i className="arrow down"></i></p>
+                </button>
+                {
+                dropdown &&
+                <div className="dropdown-menu">
+                    <Link className="header__links-search" href="/search">Search</Link>
+                    <Link className="header__links-profile" href="/my-profile">My Profile</Link>
+                    <button onClick={logout} className="btn btn-header" >Sign out</button>
+                </div>
+                }
+            </div>
+        </>
         }
     </div>
 }
